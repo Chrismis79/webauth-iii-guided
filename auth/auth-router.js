2aaf40ad37b5c27a6635830aa1238b773954b881
Chrismis79 <chrismis79@gmail.com>
2019-12-18 14:55:10 -0600

    completed in lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken')
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -25,7 +25,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+
         res.status(200).json({
+          token,
           message: `Welcome ${user.username}!`,
         });
       } else {
@@ -36,5 +39,15 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+function signToken(user){
+  const payload ={
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+  const options = {
+    expiresIn:'2hr',
+  }
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
